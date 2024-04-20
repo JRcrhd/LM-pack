3ae95efffdd68cbb17fd21b516e25dffee50641a
wjr_crhd <116958216+JRcrhd@users.noreply.github.com>
2024-04-20 20:56:20 +0800

    Add files via upload

--- overrides/kubejs/startup_scripts/script.js
+++ overrides/kubejs/startup_scripts/script.js
@@ -4,46 +4,46 @@ console.info('Hello, World! (You will only see this line once in console, during
 
 onEvent('item.registry', event => {
 	event.create('philosophy_soul', item => {
-		item.displayName('智者之魂')
+		item.displayName("智者之魂")
 		item.rarity(Rarity.EPIC)
 		item.glow(true)
-		item.tooltip('来自本我的掉落物，带有着难以言表的熟悉感……')
+		item.tooltip("A drop from the third gaia, carrying an indescribable sense of familiarity...")
 	})
 })
 
 onEvent('item.registry', event => {
 	event.create('jewel_star', item => {
-		item.displayName('宝石之星')
+		item.displayName("宝石之星")
 		item.rarity(Rarity.EPIC)
 		item.glow(false)
-		item.tooltip('在魔力洪流的冲击下，宝石的内部晶格在慢慢变化……直到融为一体。')
+		item.tooltip("Under the impact of magical torrents, the internal lattice of the gem slowly changes... until it merges into one.")
 	})
 })
 
 onEvent('item.registry', event => {
 	event.create('mini_blackhole', item => {
-		item.displayName('微缩黑洞')
+		item.displayName("微缩黑洞")
 		item.rarity(Rarity.EPIC)
 		item.glow(false)
-		item.tooltip('将此万钧之重托于掌心之时，仿佛出现了创世者托举万物的剪影……')
+		item.tooltip("When holding this weight of ten thousand jun in the palm, it feels like the silhouette of the Creator lifting the world...")
 	})
 })
 
 onEvent('item.registry', event => {
 	event.create('universe_passport', item => {
-		item.displayName('诸界认可之证')
+		item.displayName("诸界认可之证")
 		item.rarity(Rarity.EPIC)
 		item.glow(false)
-		item.tooltip('吾心吾行澄如明镜，所作所为皆为正义')
+		item.tooltip("我が心と行动に一点の曇リなし，全てが『正义』だ")
 	})
 })
 
 onEvent('item.registry', event => {
 	event.create('unawaken_creative_core', item => {
-		item.displayName('沉眠的创造术石')
+		item.displayName("沉眠的创造术石")
 		item.rarity(Rarity.EPIC)
 		item.glow(false)
-		item.tooltip('它就在这里……仿佛只差一步之遥……')
+		item.tooltip("It's right here... as if only a step away...")
 	})
 })
 
@@ -55,8 +55,8 @@ onEvent('block.registry', event => {
 
 onEvent('item.registry.armor_tiers', event => {
 	event.add('infinity',tier =>{
-		tier.slotProtections = [200,500,600,200] //靴子，护腿，胸甲，头盔
-		tier.durabilityMultiplier = 10000 //耐久倍率 基础值为 头11 胸16 腿15 鞋13
+		tier.slotProtections = [200,500,600,200]
+		tier.durabilityMultiplier = 10000
 		tier.toughness = 50
 		tier.knockbackResistance = 1
 		tier.enchantmentValue = 25
@@ -71,17 +71,17 @@ onEvent('item.registry', event => {
 	armors.forEach(a =>{
 		event.create('star_' + a ,item => {
 			item.type(a).tier('infinity')
-			item.tooltip('经过地母加持后，强大的力量化为镜面般平静的海洋。')
+			item.tooltip("After being blessed by gaia, the powerful force transforms into a mirror-like calm ocean.")
 			item.glow(true)
 			item.rarity(Rarity.EPIC)
-			item.displayName('星芒' + a)
+			item.displayName("StarLight " + a)
 		})
 	})
 })
 
 onEvent('item.tooltip', tooltip => {
-	tooltip.add(['kubejs:star_boots'],['速度','------','套装效果：','免疫普通伤害','急速治疗'])
-	tooltip.add(['kubejs:star_helmet'],['夜视','水下呼吸','------','套装效果：','免疫普通伤害', '急速治疗'])
-	tooltip.add(['kubejs:star_leggings'],['自动灭火','------','套装效果：','免疫普通伤害', '急速治疗'])
-	tooltip.add(['kubejs:star_chestplate'],['缓慢治疗','------','套装效果：','免疫普通伤害', '急速治疗'])
+	tooltip.add(['kubejs:star_boots'],['speed','------',"Set Effect:","Immunity to normal damage","Quick healing"])
+	tooltip.add(['kubejs:star_helmet'],['night vision','underwater breath','------',"Set Effect:","Immunity to normal damage","Quick healing"])
+	tooltip.add(['kubejs:star_leggings'],['auto extinguish','------',"Set Effect:","Immunity to normal damage","Quick healing"])
+	tooltip.add(['kubejs:star_chestplate'],['heal slowly','------',"Set Effect:","Immunity to normal damage","Quick healing"])
 	})
--- overrides/kubejs/startup_scripts/tooltips.js
+++ overrides/kubejs/startup_scripts/tooltips.js
@@ -1,3 +1,3 @@
 onEvent('item.tooltip', tooltip => {
-  tooltip.add(['avaritia:infinity_armor_helmet', 'avaritia:infinity_armor_boots', 'avaritia:infinity_armor_leggings', 'avaritia:infinity_armor_chestplate'], '融合后的产物，但力量还未能完全可控。若此时强行穿戴，则会被装备的力量反噬。')
+  tooltip.add(['avaritia:infinity_armor_helmet', 'avaritia:infinity_armor_boots', 'avaritia:infinity_armor_leggings', 'avaritia:infinity_armor_chestplate'], "The product of fusion, but the power is not yet fully controllable. If forcibly worn at this time, the power of the equipment will backfire.")
     })
